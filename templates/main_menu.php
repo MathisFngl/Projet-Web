9f531e42207ce3870506df8bf45f87a1fc53bc00
Mathis <98806470+MathisFngl@users.noreply.github.com>
2023-03-08 10:17:43 +0100

    menu principal

---
+++ templates/main_menu.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <metacharset="utf-8">
+    <title>Main Menu</title>
+    <link rel="stylesheet" href="../static/style/style.css">
+  </head>
+  <body>
+    Menu Principal de Jeu
+  </body>
+</html>
